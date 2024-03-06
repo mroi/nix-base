b009cda97923c9b67d2c2fe170e1b3b8fb8af423
Michael Roitzsch <reactorcontrol@icloud.com>
2024-03-06 09:11:58 +0100

    modules: assertions and warnings infrastructure

--- modules/system/activate.nix
+++ modules/system/activate.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
 
 	options = {
 		system.build = lib.mkOption {
@@ -6,14 +6,52 @@
 			type = lib.types.attrsOf lib.types.unspecified;
 			description = lib.mdDoc "Attribute set of derivations for system setup.";
 		};
+		assertions = lib.mkOption {
+			internal = true;
+			type = lib.types.listOf lib.types.unspecified;
+			default = [];
+			description = lib.mdDoc "Conditions that must hold during evaluation of the configuration.";
+		};
+		warnings = lib.mkOption {
+			internal = true;
+			type = lib.types.listOf lib.types.str;
+			default = [];
+			description = lib.mdDoc "Warnings collected during evaluation of the configuration.";
+		};
 	};
 
 	config.system.build.toplevel = pkgs.writeTextFile {
 		name = "base-activate";
 		executable = true;
-		text = ''#!/bin/sh -e
+		text = let
+
+			assertions = lib.pipe config.assertions [
+				(lib.filter (x: !x.assertion))
+				(map (x: "printError '• ${x.message}'"))
+				(x: if x == [] then [] else ([
+					"# print assertions"
+					"printError 'Failed assertions while evaluating the configuration:'"
+				] ++ x ++ [
+					"exit 1"
+				]))
+				lib.concatLines
+			];
+
+			warnings = lib.pipe config.warnings [
+				(map (x: "printWarning '• ${x}'"))
+				(x: if x == [] then [] else ([
+					"# print warnings"
+					"printWarning 'Warnings while evaluating the configuration:'"
+				] ++ x))
+				lib.concatLines
+			];
+
+		in ''#!/bin/sh -e
+			# shellcheck disable=SC2317
 			export PATH=/usr/bin:/bin:/usr/sbin:/sbin
-			${builtins.readFile ./utils.sh}
+			${lib.fileContents ./utils.sh}
+			${assertions}
+			${warnings}
 		'';
 		checkPhase = ''
 			${pkgs.stdenv.shellDryRun} "$target"

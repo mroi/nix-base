8cc6a8a08d386099ccdddb73e2a379134f7a2ab1
Michael Roitzsch <reactorcontrol@icloud.com>
2024-04-06 20:47:14 +0200

    packages: fix the Unison build on Darwin
    previously required a devShell, as the actual sandboxed build would not
    work due to xcodebuild trying to create the DerivedData folder in
    /var/empty (the home directory of the Nix build user)
    
    we now set the -derivedDataPath option for xcodebuild and tweak the
    rest of the build process to succeed

--- packages/unison.nix
+++ packages/unison.nix
@@ -1,9 +1,9 @@
 # my custom build of the unison sync tool
-{ stdenv, ocaml, xcodeenv, writeText, glibc, patchelf, unison, static ? false }:
+{ lib, stdenv, ocaml, xcodeenv, writeText, glibc, patchelf, unison, backDeploy ? false, static ? false }:
 
 if stdenv.isDarwin then (
+
 	# build Unison.app from sources
-	# TODO: xcodebuild writes to the home directory, so this only works in a nix develop shell
 	let
 		xcode = (xcodeenv.composeXcodeWrapper {
 				version = "14.2";
@@ -15,75 +15,64 @@ if stdenv.isDarwin then (
 				'';
 			});
 		# for building a back-deployable version for macOS
-#		ocaml = ocamlPackages.ocaml.overrideAttrs (attrs: {
-#			preConfigure = "MACOSX_DEPLOYMENT_TARGET=10.7";
-#		});
+		ocaml' = ocaml.overrideAttrs (attrs:
+			if backDeploy then {
+				preConfigure = "MACOSX_DEPLOYMENT_TARGET=10.7";
+			} else {}
+		);
+
 	in stdenv.mkDerivation rec {
 		pname = "unison";
 		version = unison.version;
 		src = unison.src;
 		__noChroot = true;
-		nativeBuildInputs = [ ocaml xcode ];
+		nativeBuildInputs = [ ocaml' xcode ];
 		patches = writeText "unison-fixes.patch" ''
-			--- a/src/globals.ml
-			+++ b/src/globals.ml
-			@@ -101,7 +101,7 @@ let installRoots2 () =
-			   let roots = rawRoots () in
-			   theroots :=
-			     Safelist.map Remote.canonize ((Safelist.map Clroot.parseRoot) roots);
-			-  Lwt.ignore_result (Negotiate.features (Common.sortRoots !theroots) >>= return)
-			+  Lwt_unix.run (Negotiate.features (Common.sortRoots !theroots))
-			 
-			 let roots () =
-			   match !theroots with
-			--- a/src/uimac/uimacnew.xcodeproj/project.pbxproj
-			+++ b/src/uimac/uimacnew.xcodeproj/project.pbxproj
-			@@ -647,6 +647,7 @@
-			 			baseConfigurationReference = 2E282CCC0D9AE2E800439D01 /* ExternalSettings.xcconfig */;
-			 			buildSettings = {
-			 				ALWAYS_SEARCH_USER_PATHS = NO;
-			+				ARCHS = x86_64;
-			 				CLANG_ANALYZER_LOCALIZABILITY_NONLOCALIZED = YES;
-			 				CLANG_WARN_BLOCK_CAPTURE_AUTORELEASING = YES;
-			 				CLANG_WARN_BOOL_CONVERSION = YES;
-			@@ -662,6 +663,7 @@
-			 				CLANG_WARN_STRICT_PROTOTYPES = YES;
-			 				CLANG_WARN_UNREACHABLE_CODE = YES;
-			 				CLANG_WARN__DUPLICATE_METHOD_MATCH = YES;
-			+				CODE_SIGN_INJECT_BASE_ENTITLEMENTS = NO;
-			 				ENABLE_STRICT_OBJC_MSGSEND = YES;
-			 				GCC_NO_COMMON_BLOCKS = YES;
-			 				GCC_WARN_64_TO_32_BIT_CONVERSION = YES;
-			@@ -671,6 +673,7 @@
-			 				GCC_WARN_UNUSED_FUNCTION = YES;
-			 				GCC_WARN_UNUSED_VARIABLE = YES;
-			 				LIBRARY_SEARCH_PATHS = "";
-			+				MACOSX_DEPLOYMENT_TARGET = 12.0;
-			 				PRODUCT_BUNDLE_IDENTIFIER = edu.upenn.cis.Unison;
-			 				SDKROOT = macosx;
-			 				SYSTEM_HEADER_SEARCH_PATHS = "$(OCAMLLIBDIR)";
+			--- a/src/Makefile.OCaml
+			+++ b/src/Makefile.OCaml
+			@@ -231,7 +231,7 @@
+			 	$(CC) $(CFLAGS) $(UIMACDIR)/cltool.c -o $(UIMACDIR)/build/Default/Unison.app/Contents/MacOS/cltool -framework Carbon
+			 	codesign --remove-signature $(UIMACDIR)/build/Default/Unison.app
+			 	codesign --force --sign - $(UIMACDIR)/build/Default/Unison.app/Contents/MacOS/cltool
+			-	codesign --force --sign - --entitlements $(UIMACDIR)/build/uimac*.build/Default/uimac.build/Unison.app.xcent $(UIMACDIR)/build/Default/Unison.app
+			+	codesign --force --sign - --entitlements $(NIX_BUILD_TOP)/DerivedData/Build/Intermediates.noindex/uimac*.build/Default/uimac.build/Unison.app.xcent $(UIMACDIR)/build/Default/Unison.app
+			 	codesign --verify --deep --strict $(UIMACDIR)/build/Default/Unison.app
+			 # cltool was added into the .app after it was signed, so the signature is now
+			 # broken. It must be removed, cltool separately signed, and then the entire
 		'';
-		preBuild = "unset LD";
+		preBuild = ''
+			unset LD
+			export XCODEFLAGS="-arch x86_64 -scheme uimac -configuration Default -derivedDataPath $NIX_BUILD_TOP/DerivedData"
+		'';
 		makeFlags = [
 			"-C src"
-#			"MACOSX_DEPLOYMENT_TARGET=10.7"
-#			"UISTYLE=text"
+		] ++ lib.optionals (!backDeploy) [
+			"MACOSX_DEPLOYMENT_TARGET=12.0"
+		] ++ lib.optionals backDeploy [
+			"MACOSX_DEPLOYMENT_TARGET=10.7"
+			"UISTYLE=text"
 		];
-		installPhase = "mv src/uimac/build/Default/Unison.app ./";
-		fixupPhase = "echo 'copy Unison.app to /Users/Shared/Library/CoreServices and build the UnisonIntercept project to finalize'";
+		installPhase = ''
+			mkdir -p $out/Library/CoreServices
+			cp -r src/uimac/build/Default/Unison.app $out/Library/CoreServices/
+		'';
 		meta = unison.meta;
 	}
+
 ) else (
+
 	# Linux build
 	(unison.override {
 		enableX11 = false;
 		wrapGAppsHook = null;
-	}).overrideAttrs (attrs: if static then {
-		# optionally build a static binary that can be copied to other systems
-		buildInputs = attrs.buildInputs ++ [ glibc.static ];
-		makeFlags = attrs.makeFlags ++ [ "LDFLAGS=-static" ];
-	} else {
-		# otherwise link against system C library for better system consistency
-		postFixup = "${patchelf}/bin/patchelf --remove-rpath $out/bin/*";
-	})
+	}).overrideAttrs (attrs:
+		if static then {
+			# optionally build a static binary that can be copied to other systems
+			buildInputs = attrs.buildInputs ++ [ glibc.static ];
+			makeFlags = attrs.makeFlags ++ [ "LDFLAGS=-static" ];
+		} else {
+			# otherwise link against system C library for better system consistency
+			postFixup = "${patchelf}/bin/patchelf --remove-rpath $out/bin/*";
+		}
+	)
 )

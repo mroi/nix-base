9a61bbe19eb78c6c25bc4d7a1dbdaf1e0601f0d0
Michael Roitzsch <reactorcontrol@icloud.com>
2025-01-21 09:58:04 +0100

    modules: work around Nix master error on Darwin
    current master builds of Nix create broken manifest.json files
    in profiles on Darwin

--- modules/nix/install.sh
+++ modules/nix/install.sh
@@ -134,7 +134,9 @@ if ! test -f /nix/var/nix/db/db.sqlite ; then
 		tar -x --file=nix.tar --to-stdout --wildcards nix-\*/.reginfo | trace sudo --set-home /nix/store/*-nix-*/bin/nix-store --option build-users-group nix --load-db
 	fi
 	if $isDarwin ; then
-		url=https://hydra.nixos.org/job/nix/master/binaryTarball.x86_64-darwin/latest/download/1
+#		FIXME: current master build creates broken manifest.json files in profiles on Darwin
+#		url=https://hydra.nixos.org/job/nix/master/binaryTarball.x86_64-darwin/latest/download/1
+		url=https://hydra.nixos.org/build/274231650/download/1/nix-2.25.0pre20241001_96ba7f9-x86_64-darwin.tar.xz
 		trace curl --location --output nix.tar $url
 		trace sudo tar -x --file nix.tar --directory /nix/store --gname nix --strip-components 2 nix-\*/store
 		# shellcheck disable=SC2211

9dd7da25c84e4b3919f55b3d4c5fb777627717e5
Michael Roitzsch <reactorcontrol@icloud.com>
2025-03-16 15:50:04 +0100

    rebuild: run per-package update scripts

---
+++ modules/build/versions.sh
@@ -0,0 +1,41 @@
+# update packages to current versions
+
+# shellcheck disable=SC2016,SC2154
+test "$self" || fatalError '$self is unset'
+test "$machine" || fatalError '$machine is unset'
+
+runAllUpdates() {
+	_system=$(nix eval --impure --raw --expr builtins.currentSystem)
+
+	_nixFunc() {
+		# shellcheck disable=SC2028
+		echo "x: builtins.concatStringsSep \"\\n\" (
+			builtins.attrValues (
+				builtins.mapAttrs (n: v:
+					if builtins.isString v.passthru.updateScript or null then ''
+						if \$_hasColorStdout ; then
+							printInfo \"\$(tput smul)Processing \${n}\$(tput rmul)\"
+						else
+							printInfo 'Processing \${n}'
+						fi
+						export UPDATE_NIX_ATTR_PATH=\"$1.\${n}\"
+						\${v.passthru.updateScript}
+					'' else \"\"
+				) x
+			)
+		)"
+	}
+
+	_updatesExternal=$(nix eval --quiet --no-warn-dirty --raw \
+		--apply "$(_nixFunc "packages.${_system}")" \
+		"${self}#packages.${_system}" || true)
+	_updatesInternal=$(nix eval --quiet --no-warn-dirty --raw \
+		--apply "$(_nixFunc "baseConfigurations.${machine}.config.system.build.packages")" \
+		"${self}#baseConfigurations.${machine}.config.system.build.packages" || true)
+
+	storeHeading 'Updating package versions'
+	(
+		cd "${self}" || exit
+		eval "$_updatesExternal" "$_updatesInternal"
+	)
+}

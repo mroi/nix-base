0d2dd47a30f8e6c62515c5f13e7279135c1e63c1
Michael Roitzsch <reactorcontrol@icloud.com>
2025-04-02 10:24:03 +0000

    flake: update nixpkgs
    sandbox workaround no longer required

--- machines/common.nix
+++ machines/common.nix
@@ -12,16 +12,6 @@
 		"nixpkgs#micro"
 	];
 
-	# patch from https://github.com/NixOS/nix/pull/12570 to fix sandbox exceeded error
-	nixpkgs.overlays = [ (final: prev: {
-		nix = prev.nix.overrideAttrs (lib.optionalAttrs prev.stdenv.isDarwin {
-			patches = assert prev.nix.version == "2.24.12"; builtins.fetchurl {
-				url = "https://github.com/NixOS/nix/pull/12570.diff";
-				sha256 = "1ppaml5nbi2hhn9sjczgm39s3ag0szwf2gp14yl8dv9lhv49cc2p";
-			};
-		});
-	})];
-
 	networking.firewall = lib.mkIf (pkgs.stdenv.isDarwin && config.system.systemwideSetup) {
 		allow = [
 			"/System/Library/CoreServices/UniversalControl.app/Contents/MacOS/UniversalControl"

91b465654764fa51ce97e2429d215eb016eaf1e4
Michael Roitzsch <reactorcontrol@icloud.com>
2025-05-03 21:16:00 +0200

    modules: user selection for extensions

--- modules/all.nix
+++ modules/all.nix
@@ -42,6 +42,7 @@
 	# system environment setup
 	"apps" = ./environment/apps.nix;
 	"bundles" = ./environment/bundles.nix;
+	"extensions" = ./environment/extensions.nix;
 	"hooks" = ./environment/hooks.nix;
 	"patches" = ./environment/patches.nix;
 	"profile" = ./environment/profile.nix;

2bca4b5aea3610a3107d94c26c31a7a02b08defd
Michael Roitzsch <reactorcontrol@icloud.com>
2025-05-04 19:15:04 +0200

    modules: install apps from mac app store

--- modules/environment/apps.nix
+++ modules/environment/apps.nix
@@ -9,12 +9,60 @@
 			};
 			description = "Manage Flatpak apps in system or user mode.";
 		};
+		apps = lib.mkOption {
+			type = lib.types.nullOr (lib.types.listOf (lib.getAttr pkgs.stdenv.hostPlatform.uname.system {
+				Linux = lib.types.strMatching "[a-zA-Z0-9.-]+";
+				Darwin = lib.types.int;
+			}));
+			default = [];
+			description = "List of apps to install.";
+		};
 	};
 
 	config = {
 
+		assertions = [{
+			assertion = config.environment.flatpak == "none" || pkgs.stdenv.isLinux;
+			message = "Flatpak apps are only supported on Linux";
+		}];
+
+		system.activationScripts.apps = lib.mkIf (config.environment.apps != null) (lib.stringAfter [ "volumes" ] (''
+			storeHeading 'Installing and removing applications'
+
+		'' + lib.optionalString pkgs.stdenv.isDarwin ''
+			target='${lib.concatLines (map toString config.environment.apps)}'
+			current=$(mdfind 'kMDItemAppStoreAdamID > 0' | while read -r app ; do
+				mdls -attr kMDItemAppStoreAdamID -raw "$app" ; echo
+			done)
+
+			# install missing apps
+			mas=
+			mas() { test "$mas" || mas=${pkgs.lazyBuild pkgs.mas}/bin/mas ; "$mas" "$@" ; }
+			forTarget() {
+				if ! hasLine "$current" "$1" ; then
+					trace mas install "$1"
+				fi
+			}
+			forLines "$target" forTarget
+
+			# remove unneeded apps
+			forCurrent() {
+				if ! hasLine "$target" "$1" ; then
+					location=$(mdfind "kMDItemAppStoreAdamID = $1")
+					if test -d "$location" ; then
+						trace sudo rm -rf "$location"
+					else
+						printError "Could not find app $1 at '$location'"
+					fi
+				fi
+			}
+			forLines "$current" forCurrent
+		''));
+
 		system.updateScripts.apps = lib.stringAfter [ "packages" ] (''
 			storeHeading -
+		'' + lib.optionalString pkgs.stdenv.isDarwin ''
+			trace "${pkgs.lazyBuild pkgs.mas}/bin/mas" upgrade
 		'' + lib.getAttr config.environment.flatpak {
 			system = ''
 				trace sudo flatpak update || true

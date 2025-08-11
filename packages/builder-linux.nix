b38786d0606385495bfa9ef268f776f69fa0e712
Michael Roitzsch <reactorcontrol@icloud.com>
2025-08-11 13:06:24 +0200

    packages: quality-of-life improvements for builder
    • increase disk size to allow full Linux kernel compile
    • automatically exclude disk image from Time Machine backups

--- packages/builder-linux.nix
+++ packages/builder-linux.nix
@@ -7,9 +7,14 @@
 let nixos = import "${path}/nixos" {
 
 	configuration = { lib, modulesPath, ... }: {
+
 		imports = [ "${modulesPath}/profiles/nix-builder-vm.nix" ];
-		virtualisation = {
-			host.pkgs = import path { inherit system; };
+
+		virtualisation = let hostPkgs = import path { inherit system; }; in {
+			host.pkgs = hostPkgs;
+			diskSize = lib.mkForce (128 * 1024);
+
+			# SSH access
 			forwardPorts = lib.mkForce [{
 				from = "host";
 				host.address = "127.0.0.1";
@@ -17,7 +22,23 @@ let nixos = import "${path}/nixos" {
 				guest.port = 22;
 			}];
 			sharedDirectories.keys.source = lib.mkForce "/nix/var/ssh";
+
+			# exclude QEMU disk image from Time Machine backups
+			qemu.package = hostPkgs.runCommand hostPkgs.qemu.name {} ''
+				mkdir -p $out/bin
+				ln -s ${hostPkgs.qemu}/bin/* $out/bin/
+				rm $out/bin/qemu-img
+				cat <<- 'EOF' > $out/bin/qemu-img
+					#!/bin/sh -e
+					${hostPkgs.qemu}/bin/qemu-img "$@"
+					for arg ; do
+						if test -w "$arg" ; then tmutil addexclusion "$arg" ; fi
+					done
+				EOF
+				chmod a+x $out/bin/qemu-img
+			'';
 		};
+
 		nixpkgs.hostPlatform = builtins.replaceStrings [ "darwin" ] [ "linux" ] system;
 		boot.binfmt.emulatedSystems = if binfmt then builtins.getAttr system {
 			aarch64-linux = [ "x86_64-linux" ];

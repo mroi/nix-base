5670f50037d356b652fa5aa5db78fe5a4093ff4d
Michael Roitzsch <reactorcontrol@icloud.com>
2025-12-21 12:06:48 +0100

    packages: imageoptim can use fetchzip directly

--- packages/imageoptim.nix
+++ packages/imageoptim.nix
@@ -1,30 +1,26 @@
 # ImageOptim image optimizer
-{ lib, stdenvNoCC, fetchzip }:
+{ lib, fetchzip }:
 
-stdenvNoCC.mkDerivation rec {
+fetchzip rec {
 	pname = "imageoptim";
 	version = "1.9.3";
 
-	src = fetchzip {
-		url = "https://imageoptim.com/ImageOptim${version}.tar.xz";
-		hash  = "sha256-DTgGypH+lyc4T+WqSijEute5meNeZ8l2O1BPYm1rL+M=";
-		stripRoot = false;
-	};
+	url = "https://imageoptim.com/ImageOptim${version}.tar.xz";
+	hash = "sha256-++nXdYtWSFSh1rW07ubQIiayOVvNCiDGEl9u7ZAdK9o=";
+	stripRoot = false;
 
-	__noChroot = true;
-	installPhase = ''
+	postFetch = ''
 		mkdir -p $out/Applications
 		mv ImageOptim.app $out/Applications/
 	'';
-	dontFixup = true;
 
 	passthru.updateScript = ''
 		version=$(curl --silent https://imageoptim.com/appcast.xml | xmllint --xpath 'string(/rss/channel/item/enclosure/@*[local-name()="version"])' -)
 		updateVersion version "$version"
 		if didUpdate ; then
 			curl --silent --output ImageOptim.tar.xz "https://imageoptim.com/ImageOptim''${version}.tar.xz"
-			mkdir root
-			tar -xf ImageOptim.tar.xz --directory root
+			mkdir -p root/Applications
+			tar -xf ImageOptim.tar.xz --directory root/Applications
 			if $isDarwin && checkSig root/ImageOptim.app 59KZTZA4XR ; then
 				updateHash hash "$(nix hash path root)"
 			else

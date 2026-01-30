776898d96e6e6b8870931de85a7c6c677b1d58e9
Michael Roitzsch <reactorcontrol@icloud.com>
2026-01-30 07:18:19 +0100

    modules: extend packages to include recommends
    dpkg has optional ‘recommended’ dependencies,
    users can choose to install those

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,7 +1,16 @@
 { config, lib, pkgs, ... }: {
 
 	options.system.packages = lib.mkOption {
-		type = lib.types.nullOr (lib.types.listOf lib.types.str);
+		type = lib.types.nullOr (lib.types.listOf (lib.types.either
+			lib.types.str
+			(lib.types.submodule { options = {
+				name = lib.mkOption {
+					type = lib.types.str;
+					description = "The name of the package.";
+				};
+				includeRecommends = lib.mkEnableOption "recommended dependencies";
+			};})
+		));
 		default = [];
 		description = "System-level packages to install in the underlying system.";
 	};
@@ -15,29 +24,32 @@
 
 		system.activationScripts.packages = let
 
+			packages = map (x: if lib.isString x then
+				{ name = x; includeRecommends = false; } else x) config.system.packages;
+
 			installScript = package: lib.optionalString pkgs.stdenv.isLinux ''
-				if ! dpkg --status ${package} > /dev/null 2>&1 ; then
-					trace sudo apt-get install --no-install-recommends ${package}
+				if ! dpkg --status ${package.name} > /dev/null 2>&1 ; then
+					trace sudo apt-get install ${if package.includeRecommends then "" else "--no-install-recommends "}${package.name}
 				fi
 			'';
 
 		in ''
 			storeHeading 'Installing packages in the underlying system'
 
-			${lib.concatLines (map installScript config.system.packages)}
+			${lib.concatLines (map installScript packages)}
 		'';
 
 		system.updateScripts.packages = lib.optionalString pkgs.stdenv.isLinux ''
 			storeHeading -
 			trace sudo apt-get update --quiet
-			trace sudo apt-get dist-upgrade --no-install-recommends || true
-			trace sudo apt-get autoremove --purge || true
+			trace sudo apt-get dist-upgrade || true
+			trace sudo apt-get autopurge || true
 			trace sudo apt-get clean
 		'';
 
 		system.cleanupScripts.packages = lib.stringAfter [ "volumes" ] (lib.optionalString pkgs.stdenv.isLinux ''
 			storeHeading 'Cleaning system-level packages'
-			trace sudo apt-get --assume-yes autoremove --purge
+			trace sudo apt-get --assume-yes autopurge
 			trace sudo apt-get clean
 			trace sudo apt-cache gencaches
 			trace sudo apt-get --quiet check

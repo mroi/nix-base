4f0199ce6633e06e85c80ae2b4f827a7dd80e728
Michael Roitzsch <reactorcontrol@icloud.com>
2026-05-25 12:44:40 +0200

    machines: changes to GitHub runner certificates

--- machines/common.nix
+++ machines/common.nix
@@ -33,8 +33,8 @@
 	security.pki.certificateTrust.system = lib.mkIf (pkgs.stdenv.isDarwin && config.system.systemwideSetup) {
 		# DigiCert High Assurance EV Root CA: involved in geo services and commerce
 		"5FB7EE0633E259DBAD0C4C9AE6D38F1A61C7DC25" = { basicX509 = true; sslServer = true; timeStamping = true; };
-		# USERTrust RSA Certification Authority: basicX509 needed by GitHub action runner
-		"2B8F1B57330DBBA2D07A6C51F70EE90DDAB9AD8E" = { basicX509 = true; sslServer = true; };
+		# USERTrust RSA Certification Authority
+		"2B8F1B57330DBBA2D07A6C51F70EE90DDAB9AD8E" = { sslServer = true; };
 		# T-TeleSec GlobalRoot Class 2
 		"590D2D7D884F402E617EA562321765CF17D894E9" = { sslServer = true; };
 		# DigiCert Global Root G3
@@ -47,14 +47,14 @@
 		"A8985D3A65E5E5C4B2D7D66D40C6DD2FB19C5436" = { sslServer = true; };
 		# GlobalSign Root CA
 		"B1BC968BD4F49D622AA89A81F2150152A41D829C" = { sslServer = true; };
-		# ISRG Root X1: Let’s encrypt
-		"CABD2A79A1076A31F21D253635CB039D4329A5E8" = { sslServer = true; };
+		# ISRG Root X1: Let’s encrypt, basicX509 needed by GitHub action runner
+		"CABD2A79A1076A31F21D253635CB039D4329A5E8" = { basicX509 = true; sslServer = true; };
 		# AAA Certificate Services: some Apple Server certs
 		"D1EB23A46D17D68FD92564C2F1F1601764D8E349" = { sslServer = true; };
 		# GlobalSign
 		"D69B561148F01C77C54578C10926DF5B856976AD" = { sslServer = true; };
-		# DigiCert Global Root G2: basicX509 needed by GitHub action runner
-		"DF3C24F9BFD666761B268073FE06D1CC8D4F82A4" = { basicX509 = true; sslServer = true; };
+		# DigiCert Global Root G2
+		"DF3C24F9BFD666761B268073FE06D1CC8D4F82A4" = { sslServer = true; };
 	};
 
 	environment.extensions = lib.mkIf pkgs.stdenv.isDarwin {

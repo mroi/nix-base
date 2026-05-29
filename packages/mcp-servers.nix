d13d93ba31cb20fd4c1961fc1003830d20d3bf51
Michael Roitzsch <reactorcontrol@icloud.com>
2026-05-29 13:27:51 +0200

    packages: add curated MCP servers
    • apple developer documentation
    • nixpkgs and nixos information
    • generic web search using DuckDuckGo

--- packages/mcp-servers.nix
+++ packages/mcp-servers.nix
@@ -1,5 +1,10 @@
 # a curated set of model-context-protocol (MCP) servers
-{ lib, stdenv, writeShellScriptBin, nodejs-slim, uv, }: let
+{ lib, stdenv, writeShellScriptBin, nodejs-slim, uv, mcp-nixos, fetchFromGitHub, swift, git, cacert }: let
+
+	versions = {
+		duckduckgo = "0.4.0";
+		apple-docs = "1.0.1";
+	};
 
 	# cut-off time for auto-loaded package dependencies to get them somewhat pinned
 	depsTimestamp = "1779536132";
@@ -11,6 +16,39 @@
 	# run packages from NPM or PyPI using the dependency cutoff to pin dependencies as best as possible
 	npx = "${nodejs-slim.npm}/bin/npx --yes --min-release-age $(( ($(date +%s) - ${depsTimestamp} + 43200) / 86400 ))";
 	uvx = "${uv}/bin/uvx --exclude-newer=${depsDate}";
+
+	servers = {
+
+		duckduckgo = "exec ${uvx} duckduckgo-mcp-server==${versions.duckduckgo} \"$@\"";
+
+	} // lib.optionalAttrs stdenv.isDarwin {
+
+		apple-docs = "exec ${apple-docs}/bin/Apple-Docs-MCP \"$@\"";
+
+	} // lib.optionalAttrs (stdenv.system != "x86_64-darwin") {
+
+		nixos = "exec ${mcp-nixos}/bin/mcp-nixos \"$@\"";
+	};
+
+	apple-docs = stdenv.mkDerivation {
+		pname = "apple-docs-mcp";
+		version = versions.apple-docs;
+		src = fetchFromGitHub {
+			owner = "justindal";
+			repo = "Apple-Docs-MCP";
+			rev = "v${versions.apple-docs}";
+			hash = "sha256-Yvx+ybPHeLTKo/orCwatVi4Zq7nyoA3BxaAVysh9Ef0=";
+		};
+		patches = ./mcp-apple-docs.patch;
+		__noChroot = true;
+		# move the build to packaged swift once version 6 is available
+		nativeBuildInputs = assert lib.versionOlder swift.version "6"; [ git cacert ];
+		buildPhase = ''
+			unset DEVELOPER_DIR SDKROOT
+			/usr/bin/swift build --disable-sandbox --configuration release
+		'';
+		installPhase = "mkdir -p $out/bin ; cp .build/release/Apple-Docs-MCP $out/bin/";
+	};
 
 in (writeShellScriptBin "mcp-servers" ''
 
@@ -62,6 +100,13 @@ in (writeShellScriptBin "mcp-servers" ''
 			version=$(curl --silent "https://pypi.org/pypi/$2/json" | jq --raw-output .info.version)
 			updateVersion "$1" "$version"
 		}
+
+		updatePyPI duckduckgo duckduckgo-mcp-server
+
+		# apple-docs
+		release=$(curl --silent https://api.github.com/repos/justindal/Apple-Docs-MCP/releases/latest | jq --raw-output .name)
+		version=''${release#v}
+		updateVersion apple-docs "$version"
 
 		# dependency cutoff time
 		timestamp=$(jq .nodes.nixpkgs.locked.lastModified "$_self/flake.lock")

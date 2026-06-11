129822d197d1716de14d3a0c85d34ca8c1dd1e35
Michael Roitzsch <reactorcontrol@icloud.com>
2026-06-11 14:19:50 +0200

    modules: update for macOS 27 Golden Gate
    • remove some missing extensions
    • remove missing firewall executable
    • fix OpenCode binary signature

--- packages/opencode.nix
+++ packages/opencode.nix
@@ -1,7 +1,28 @@
 # set environment variables to consolidate state in ~/.local/state/opencode
-{ opencode, runCommand, callPackage, mcp-servers ? callPackage (import ./mcp-servers.nix) {} }:
+{ stdenv, opencode, runCommand, callPackage, mcp-servers ? callPackage (import ./mcp-servers.nix) {} }:
 
-runCommand "opencode" { inherit (opencode) name; } ''
+let
+	# bun creates an opencode binary with a broken signature, macOS ≥ 27 rejects this
+	# https://github.com/oven-sh/bun/issues/32159
+	opencode' = if stdenv.isDarwin then opencode.overrideAttrs (attrs: {
+		__noChroot = true;
+		# do not run smoke test, because the binary is still broken here
+		postPatch = attrs.postPatch + ''
+			substituteInPlace packages/opencode/script/build.ts --replace-fail \
+				'if (item.os === process.platform && item.arch === process.arch && !item.abi) {' \
+				'if (false) {'
+		'';
+		# fix the signature by resigning
+		postBuild = ''
+			if /usr/bin/codesign --verify dist/opencode-darwin-arm64/bin/opencode 2> /dev/null ; then
+				echo 'codesigning works correctly now, remove workaround'
+				exit 1
+			fi
+			/usr/bin/codesign --force --sign - dist/opencode-darwin-arm64/bin/opencode
+		'';
+	}) else opencode;
+
+in runCommand "opencode" { inherit (opencode) name; } ''
 	mkdir -p $out/bin
 	cat <<- EOF > $out/bin/opencode
 		#!/bin/sh
@@ -22,10 +43,10 @@ runCommand "opencode" { inherit (opencode) name; } ''
 
 		# run opencode
 		if \$tui ; then
-			${opencode}/bin/opencode "\$@"
+			${opencode'}/bin/opencode "\$@"
 			clear
 		else
-			exec ${opencode}/bin/opencode "\$@"
+			exec ${opencode'}/bin/opencode "\$@"
 		fi
 	EOF
 	chmod a+x $out/bin/opencode
